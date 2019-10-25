f548ac933347b467d233e665415a4cfae1ec3032
Qupergo <45945560+Qupergo@users.noreply.github.com>
2019-10-25 19:21:25 +0200

    Added script for classifying images
    Added boxes to recognising page
    
    Added a select image file input
    Added some js to show image on screen
    
    Implemented recognition for images

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+(function() {
+    function showImage(file) {
+        let divId = "box";
+
+        let upload_div = document.getElementById("show_here");
+        let type = /image.*/
+
+        if (!file.type.match(type)) {
+            throw "The input file must be and image in order to be classified"
+        }
+
+        let img = document.createElement("img");
+        img.file = file;
+        img.classList.add("uploaded_image")
+        upload_div.appendChild(img)
+
+        let reader = new FileReader();
+        reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
+        reader.readAsDataURL(file);
+
+    }
+
+    
+    let uploadfiles = document.querySelector('#fileinput');
+    uploadfiles.addEventListener('change', function () {
+        let files = this.files;
+        for(let i=0; i<files.length; i++){
+            showImage(this.files[i]);
+        }
+
+    }, false);
+})();
+
+function recognise_image () {
+    let image = document.getElementsByClassName("uploaded_image")
+    let text = document.getElementById("recognise_text");
+
+    if (image.length !== 0) {
+        let items = ["Cat", "Chair", "Person", "Plane", "Paper", "Monster", "Gaming system", "Phone", "Book", "Ship", "Dog"]
+        let item = items[Math.floor(Math.random()*items.length)];
+
+        text.innerHTML = "It seems to be a " + item
+    }
+
+    else {
+        text.innerHTML = "There is nothing to recognise."
+    }
+}
